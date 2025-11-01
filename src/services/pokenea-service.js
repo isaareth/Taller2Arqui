eac6fbae33edd223f5c7bd7b79f4fb27429ac786
Sofía Isaareth Flores Suárez <sifloress@eafit.edu.co>
2025-10-31 20:01:58 -0500

    Feat: Create pokenea services

---
+++ src/services/pokenea-service.js
@@ -0,0 +1,31 @@
+const pokeneas = require('../models/pokenea-data');
+const os = require('os');
+
+const getRandomPokeneaInstance = () => {
+  const index = Math.floor(Math.random() * pokeneas.length);
+  return pokeneas[index];
+};
+
+
+exports.getRandomPokenea = () => {
+  const pokenea = getRandomPokeneaInstance();
+
+  return {
+    id: pokenea.getId(),
+    name: pokenea.getName(),
+    height: pokenea.getHeight(),
+    skill: pokenea.getSkill(),
+    containerId: os.hostname()
+  };
+};
+
+
+exports.getRandomImageAndPhrase = () => {
+  const pokenea = getRandomPokeneaInstance();
+
+  return {
+    image: pokenea.getImage(),
+    phrase: pokenea.getPhrase(),
+    containerId: getContainerId()
+  };
+};
